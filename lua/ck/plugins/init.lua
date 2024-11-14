cb448909b2c5f5bf9a0de79f8b75f8cecf23d38f
Cenk Kılıç <cenk@kilic.dev>
2024-11-14 14:25:42 +0100

    fix: disable thing

--- lua/ck/plugins/init.lua
+++ lua/ck/plugins/init.lua
@@ -71,7 +71,7 @@ local plugins = {
   "gitignore-nvim",
   "lspsaga-nvim",
   -- "tiny-code-action-nvim",
-  "tiny-inline-diagnostic-nvim",
+  -- "tiny-inline-diagnostic-nvim",
   "nvim-dap-ui",
   "nvim-dap-virtual-text",
   "dressing",
