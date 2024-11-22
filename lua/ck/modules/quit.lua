0800db807e76df12bf7f2c86b7a68eaaddee677e
Cenk Kılıç <cenk@kilic.dev>
2024-11-22 01:34:10 +0100

    fix: add confirm func

--- lua/ck/modules/quit.lua
+++ lua/ck/modules/quit.lua
@@ -3,28 +3,33 @@ local M = {}
 --- Safely quits the workspace.
 function nvim.fn.workspace_quit()
   local buffers = vim.api.nvim_list_bufs()
-  local modified = false
+  local modified = {}
   for _, bufnr in ipairs(buffers) do
     if vim.api.nvim_get_option_value("buflisted", { buf = bufnr }) and vim.api.nvim_get_option_value("modified", { buf = bufnr }) then
-      modified = true
-
-      break
+      table.insert(modified, require("ck.utils.fs").get_project_buffer_filepath(bufnr))
     end
   end
 
-  if modified then
-    vim.ui.select({ { name = "yes", value = true }, { name = "no", value = false } }, {
-      prompt = "You have unsaved changes. Quit anyway? (y/n) ",
-      format_item = function(item)
-        return item.name
-      end,
-    }, function(input)
-      if not input then
-        return
-      end
-
-      vim.cmd("qa!")
-    end)
+  if #modified > 0 then
+    require("ck.utils").ui_confirm({
+      prompt = ("You have unsaved changes, quit anyways?\n%s"):format(table.concat(modified, "\n")),
+      choices = {
+        {
+          label = "Quit",
+          callback = function()
+            vim.cmd("qa!")
+          end,
+        },
+        {
+          label = "Save",
+          callback = function()
+            vim.cmd("wa")
+            vim.cmd("qa!")
+          end,
+        },
+        { label = "Cancel" },
+      },
+    })
   else
     vim.cmd("qa!")
   end
--- lua/ck/utils/init.lua
+++ lua/ck/utils/init.lua
@@ -70,4 +70,27 @@ function M.get_visual_selection()
   end
 end
 
+---@param opts { prompt: string, choices: { label: string, callback?: fun(): nil }[] }
+function M.ui_confirm(opts)
+  local choice = vim.fn.confirm(
+    opts.prompt,
+    table.concat(
+      vim.tbl_map(function(choice)
+        return "&" .. choice.label
+      end, opts.choices),
+      "\n"
+    )
+  )
+
+  if choice == 0 then
+    return
+  end
+
+  local cb = opts.choices[choice].callback
+
+  if type(cb) == "function" then
+    cb()
+  end
+end
+
 return M
