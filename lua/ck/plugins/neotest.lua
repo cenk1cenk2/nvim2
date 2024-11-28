d5dab53e56b0f6fe9beec20e1579d5cbaa349822
Cenk Kılıç <cenk@kilic.dev>
2024-11-28 15:57:27 +0100

    fix: update neotest config

--- lua/ck/plugins/neotest.lua
+++ lua/ck/plugins/neotest.lua
@@ -65,6 +65,24 @@ function M.config()
         floating = {
           border = nvim.ui.border,
         },
+        -- See all config options with :h neotest.Config
+        discovery = {
+          -- Drastically improve performance in ginormous projects by
+          -- only AST-parsing the currently opened buffer.
+          enabled = false,
+          -- Number of workers to parse files concurrently.
+          -- A value of 0 automatically assigns number based on CPU.
+          -- Set to 1 if experiencing lag.
+          concurrent = 0,
+        },
+        running = {
+          -- Run tests concurrently when an adapter provides multiple commands to run.
+          concurrent = true,
+        },
+        summary = {
+          -- Enable/disable animation of icons.
+          animated = false,
+        },
         summary = {
           mappings = {
             expand = { "<CR>", "<2-LeftMouse>" },
