f8063ef55bf0a99fdb723cbd809fae824b8fa327
Cenk Kılıç <cenk@kilic.dev>
2024-12-16 16:33:26 +0100

    fix: drop log level

--- lua/ck/plugins/tmux-toggle-popup-nvim.lua
+++ lua/ck/plugins/tmux-toggle-popup-nvim.lua
@@ -26,8 +26,8 @@ function M.config()
     setup = function()
       ---@type tmux-toggle-popup.Config
       return {
-        log_level = vim.log.levels.DEBUG,
-        -- log_level = require("ck.log"):to_nvim_level(),
+        -- log_level = vim.log.levels.DEBUG,
+        log_level = require("ck.log"):to_nvim_level(),
         env = {
           VISUAL = function()
             return vim.env["VISUAL"]
