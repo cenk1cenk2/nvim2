189998830a02f37e9ba0dfbc1618d45c4ce08b95
Cenk Kılıç <cenk@kilic.dev>
2024-12-31 10:58:54 +0100

    fix: update neovim configuration

--- lua/ck/plugins/neogit.lua
+++ lua/ck/plugins/neogit.lua
@@ -17,9 +17,38 @@ function M.config()
         },
       }
     end,
+    configure = function(config, fn)
+      fn.setup_callback(require("ck.plugins.possession-nvim").name, function(c)
+        local before_save = c.hooks.before_save
+        c.hooks.before_save = function(name)
+          pcall(function()
+            if is_loaded("neogit") then
+              require("neogit").close()
+            end
+          end)
+
+          return before_save(name)
+        end
+
+        return c
+      end)
+    end,
     setup = function()
       return {
         graph_style = "kitty",
+        disable_insert_on_commit = true,
+        git_services = {
+          ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+          ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
+          ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+          ["azure.com"] = "https://dev.azure.com/${owner}/_git/${repository}/pullrequestcreate?sourceRef=${branch_name}&targetRef=${target}",
+          ["gitlab.kilic.dev"] = "https://gitlab.kilic.dev/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+          ["gitlab.common.cloud.riag.digital"] = "https://gitlab.common.cloud.riag.digital/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+        },
+        -- Disable line numbers
+        disable_line_numbers = false,
+        -- Disable relative line numbers
+        disable_relative_line_numbers = false,
       }
     end,
     on_setup = function(c)
