7d407bc73bf30f066a441c507b28188a85808c96
Cenk Kılıç <cenk@kilic.dev>
2025-01-08 13:59:53 +0100

    fix: update plugin configuration

--- lua/ck/plugins/blink-cmp.lua
+++ lua/ck/plugins/blink-cmp.lua
@@ -97,8 +97,7 @@ function M.config()
               "npm",
               "git",
               "go_pkgs",
-              "luasnip",
-              -- "snippets",
+              "snippets",
               "path",
               "buffer",
               "ripgrep",
@@ -243,19 +242,20 @@ function M.config()
           },
         },
         snippets = {
-          expand = function(snippet)
-            require("luasnip").lsp_expand(snippet)
-          end,
-          active = function(filter)
-            if filter and filter.direction then
-              return require("luasnip").jumpable(filter.direction)
-            end
-
-            return require("luasnip").in_snippet()
-          end,
-          jump = function(direction)
-            require("luasnip").jump(direction)
-          end,
+          preset = "luasnip",
+          -- expand = function(snippet)
+          --   require("luasnip").lsp_expand(snippet)
+          -- end,
+          -- active = function(filter)
+          --   if filter and filter.direction then
+          --     return require("luasnip").jumpable(filter.direction)
+          --   end
+          --
+          --   return require("luasnip").in_snippet()
+          -- end,
+          -- jump = function(direction)
+          --   require("luasnip").jump(direction)
+          -- end,
         },
         appearance = {
           kind_icons = nvim.ui.icons.kind,
