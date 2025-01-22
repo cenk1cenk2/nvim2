5b730603fe8e797ac4e1cb5fd6cb3aa0e3c0085a
Cenk Kılıç <cenk@kilic.dev>
2025-01-22 09:15:34 +0100

    fix: update plugin

--- lua/ck/plugins/nvim-lspconfig.lua
+++ lua/ck/plugins/nvim-lspconfig.lua
@@ -10,8 +10,6 @@ function M.config()
       return {
         "neovim/nvim-lspconfig",
         event = { "BufReadPost", "BufNewFile", "BufNew" },
-        -- https://github.com/folke/neoconf.nvim/issues/104
-        commit = "46de45200afab642a134cf4e26f8f495ab7a49dd",
         init = false,
         config = function()
           require("ck.lsp").setup()
