f28a6efe358b484f449d82f736aee86a7baa89ba
Cenk Kılıç <cenk@kilic.dev>
2025-02-06 12:55:19 +0100

    fix: update ai configuration

--- lua/ck/override.lua
+++ lua/ck/override.lua
@@ -27,14 +27,19 @@ nvim.lsp.copilot.filetypes = {
   cvs = false,
   ["."] = false,
 }
-nvim.lsp.ai.provider = "copilot"
--- nvim.lsp.ai.provider = "ai.kilic.dev"
+-- nvim.lsp.ai.provider = "copilot"
+nvim.lsp.ai.provider = "ai.kilic.dev"
 nvim.lsp.ai.debug = false
 nvim.lsp.ai.model.completion = "qwen2.5-coder:7b"
 nvim.lsp.ai.model.chat = "qwen2.5-coder:7b"
-nvim.lsp.ai.filetypes.enabled = { "*" }
-nvim.lsp.ai.filetypes.ignored = {}
-
+nvim.lsp.ai.filetypes.enabled = {
+  "*",
+}
+nvim.lsp.ai.filetypes.ignored = {
+  "Telescope",
+  "TelescopePrompt",
+  "TelescopeResults",
+}
 nvim.lsp.ensure_installed = {
   ---- language servers
   "ansiblels",
