82707bcae89570faed57866704553335d70188e6
Cenk Kılıç <cenk@kilic.dev>
2025-02-09 01:56:24 +0100

    fix: update variables

--- lua/ck/override.lua
+++ lua/ck/override.lua
@@ -29,16 +29,18 @@ nvim.lsp.copilot.filetypes = {
 }
 
 nvim.lsp.ai.debug = false
+
 nvim.lsp.ai.provider.completion = "copilot"
-nvim.lsp.ai.provider.chat = "copilot"
 nvim.lsp.ai.model.completion = "deepseek-coder-v2:16b"
-nvim.lsp.ai.model.chat = "deepseek-coder-v2:16b"
 nvim.lsp.ai.completion.number_of_completions = 1
 nvim.lsp.ai.completion.line_limit = -1
-nvim.lsp.ai.chat.context_window = 1024 * 4
 nvim.lsp.ai.completion.context_window = 1024 * 4
 nvim.lsp.ai.completion.vectorcode.enabled = true
 nvim.lsp.ai.completion.vectorcode.number_of_files = 10
+
+nvim.lsp.ai.provider.chat = "copilot"
+nvim.lsp.ai.model.chat = "deepseek-coder-v2:16b"
+nvim.lsp.ai.chat.context_window = 1024 * 4
 
 nvim.lsp.ai.filetypes.enabled = {
   "*",
